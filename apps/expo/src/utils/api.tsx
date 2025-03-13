82d68fcb844e8c6080d13befad8c0a3d83d79829
Laurent De Marez <laurent@demarez.org>
2025-03-13 17:12:41 +0100

    chore: rename api package to trpc

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -3,7 +3,7 @@ import { createTRPCClient, httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import superjson from "superjson";
 
-import type { AppRouter } from "@reciperun/api";
+import type { AppRouter } from "@reciperun/trpc";
 
 import { getBaseUrl } from "./base-url";
 import { getToken } from "./session-store";
@@ -46,4 +46,4 @@ export const trpc = createTRPCOptionsProxy<AppRouter>({
   queryClient,
 });
 
-export { type RouterInputs, type RouterOutputs } from "@reciperun/api";
+export { type RouterInputs, type RouterOutputs } from "@reciperun/trpc";
